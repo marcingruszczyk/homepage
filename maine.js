e68edbf604c258e5c8b034a6c3402524244178da
Marcin Gruszczyk <marcin.gruszczyk95@gmail.com>
2021-03-03 12:19:28 +0100

    Use BEM & Few changes in CSS & Use JS to fill section.

--- maine.js
+++ maine.js
@@ -1,3 +1,7 @@
 const firstName = 'Marcin';
 const age = 26;
 console.log(`Witaj nieznajomy!! Ja jestem ${firstName} i jestem początkującym programistą. Mam ${age} i mam duże nadzieje, że osiągnę sukces w tej dziedzinie.`);
+
+const descriptionJs = document.querySelector('.section__description--javascript')
+
+descriptionJs.innerHTML = 'Treść tej sekcji jest automatycznie uzupeniania przez skrypt napisany w Javascript.';
